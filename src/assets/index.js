af89ec69bdb41f1aa1de0d679df57b3d07183b46
Evelyn Nguyen <evelyn@Evelyns-MacBook-Air.local>
2025-09-02 21:03:33 -0400

    updated code implemented API

---
+++ src/assets/index.js
@@ -0,0 +1,19 @@
+import search_icon from './search.png';
+import clear from './clear.png';
+import cloud from './cloud.png';
+import drizzle from './drizzle.png';
+import humidity from './humidity.png';
+import rain from './rain.png';
+import snow from './snow.png';
+import wind from './wind.png';
+
+export {
+  search_icon,
+  clear,
+  cloud,
+  drizzle,
+  humidity,
+  rain,
+  snow,
+  wind
+};
--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,17 +1,51 @@
 import React from 'react'
 import "./Weather.css"
-import search_icon from '../assets/search.png'
+import { search_icon, clear, cloud, drizzle, humidity, rain, snow, wind} from '../assets';
 
 const Weather = () => {
-  return (
-    <div className ='weather'>
+    const search= async (city) => {
+        try {
+            const url =  `https://api.openweathermap.org/data/2.5/weather?q= ${city}&appid=${import.meta.env.VITE_APP_ID}`;
+
+        } catch (error){
+
+        }
         
+    }
+
+
+
+  return (
+
+    <div className ='weather'>
+       
         <div className='search-bar'>
             <input type = 'text' placeholder='Search'/>
             <img src={search_icon} alt="" />
-        
         </div>
-      
+
+        <img src= {clear} alt="" className='weather-icon' />
+        <p className='temperature '>16 degrees</p>
+        <p className='location'> London</p>
+        
+        <div className='weather-data'>
+            <div className='column'>
+                <img src={humidity} alt="" />
+                <div>
+                    <p>91%</p>
+                    <span>Humidity</span>
+                </div>
+            </div>
+            <div className='column'>
+                <img src={wind} alt="" />
+                <div>
+                    <p>3.6km/h</p>
+                    <span>Wind</span>
+                </div>
+            </div>
+
+        </div>
+    
     </div>
   )
 }
